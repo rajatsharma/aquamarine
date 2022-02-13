dc5e0e921cb18889a2f001b6d09434ecc2065bc0
Rajat Sharma <lunasunkaiser@gmail.com>
2022-02-14 00:30:21 +0530

    First commit

---
+++ src/Aquamarine.hs
@@ -0,0 +1,15 @@
+module Aquamarine where
+
+import Soothsayer ((***))
+
+abbr :: String -> String -> String
+abbr abbreviation fullCommand = "abbr -a -g {0} {1}" *** [abbreviation, fullCommand]
+
+fishAddPath :: String -> String
+fishAddPath path = "fish_add_path {0}" *** [path]
+
+functionWithDescription :: String -> String -> [String] -> String
+functionWithDescription name description body = "function {0} -d {1}\n\t{2}\nend" *** [name, description, unlines body]
+
+function :: String -> [String] -> String
+function name body = "function {0}\n\t{2}\nend" *** [name, unlines body]
